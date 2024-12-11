b64263a8ecefb6f823a15fac52df0c41d82b093f
Chukwudera Amalu <chukwuderaamalu@gmail.com>
2024-12-11 15:39:37 +0100

    reg form assignment

--- main.js
+++ main.js
@@ -7,16 +7,33 @@ let fullname = document.querySelector(`.fullname`);
 let number = document.querySelector(`.number`);
 let email = document.querySelector(`.email`);
 let data = document.querySelector(`.haba`);
+let bott = document.querySelector(`.bott`);
 
 
 
 
 function myGirl () {
-    fullname.innerHTML = ` Full Name: ${fName.value}`
+    
+
+     if (bott.innerHTML === "Submit") {
+        bott.innerHTML = `Submitted`
+        fullname.innerHTML = ` Full Name: ${fName.value}`
     email.innerHTML =  `Email Address : ${emAddress.value}`
     number.innerHTML = `Phone Number : ${phNumber.value}`
 data.innerHTML = ` Hi,${fName.value} your details:`
     
      submitted.innerHTML = `You have successfully submitted your data.Thank you!`
      alert(`Click "OK" to continue!`)
+     }
+     else{
+        bott.innerHTML = "Submit"
+        fName.value = ``
+        emAddress.value = ``
+        phNumber.value = ``
+        data.innerHTML = `No details submitted yet!`
+        fullname.innerHTML = ``
+        email.innerHTML = ``
+        number.innerHTML = ``
+        submitted.innerHTML = `Please fill the form!`
+     }
 }
